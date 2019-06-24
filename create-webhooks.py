afb02e5a10e864ede9608eb5f662c20e1960b665
Amina Dieng <adieng@DCP-APPLE-1786.local>
2019-06-24 14:22:08 -0400

    created push event hook

--- create-webhooks.py
+++ create-webhooks.py
@@ -2,28 +2,39 @@
 
 import csv
 import os
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-REPO_LIST = './csv/repos.txt'
+REPO_LIST = './csv/test-repos.txt'
 REPO_OWNER = 'NYCPlanning'
 TOKEN = os.environ['GITHUB_PERSONAL_ACCESS_TOKEN']
 HEADER = {'Authorization': 'token ' + TOKEN, 'Accept': 'application/vnd.github.symmetra-preview+json'}
+CONFIG = {"url":"http://plannerbot.planninglabs.nyc/hubot/github-repo-listener"}
+
+def create_repo_hook(repo):
+    url = 'https://api.github.com/repos/%s/%s/hooks' % (REPO_OWNER, repo)
+    # webhook triggered by all repo events (creation, deletion, renamed, etc.)
+    events = ['repository']
+    params = {"config":CONFIG, "events":events}
+    response = requests.post(url, json=params, headers=HEADER)
+
+def create_push_hook(repo):
+    url = 'https://api.github.com/repos/%s/%s/hooks' % (REPO_OWNER, repo)
+    # webhook triggered by push to a repo
+    events = ['push']
+    params = {"config":CONFIG, "events":events}
+    response = requests.post(url, json=params, headers=HEADER) 
 
 
-# def create_hook(repo):
-# 	CREATE_URL = 'https://api.github.com/repos/%s/%s/labels' % (REPO_OWNER, repo)
-#     PARAMS = {"name":TO_CREATE[j], "description":DESC[j], "color":COLORS[j]}
-#     response = requests.post(CREATE_URL, json=PARAMS, headers=HEADER)
-
-# with open(REPO_LIST) as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count == 0:
-#             line_count+=1
-#         else:
-#             repo_name = row[0]
-#             delete_labels(repo_name)
-#             create_labels(repo_name)
-#             line_count+=1
+with open(REPO_LIST) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count+=1
+        else:
+            repo_name = row[0]
+            create_repo_hook(repo_name)
+            create_push_hook(repo_name)
+            line_count+=1
