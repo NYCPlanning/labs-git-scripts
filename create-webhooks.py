c4a040d9d4fe7e114276306f9426542b6bf4188b
Amina Dieng <adieng@DCP-APPLE-1786.local>
2019-06-28 11:20:58 -0400

    consolidated webhooks

--- create-webhooks.py
+++ create-webhooks.py
@@ -4,28 +4,20 @@ import csv
 import os
 import requests
 from dotenv import load_dotenv
-
 load_dotenv()
+
 REPO_LIST = './csv/repos.txt'
 REPO_OWNER = 'NYCPlanning'
 TOKEN = os.environ['GITHUB_PERSONAL_ACCESS_TOKEN']
 HEADER = {'Authorization': 'token ' + TOKEN, 'Accept': 'application/vnd.github.symmetra-preview+json'}
-CONFIG = {"url":"http://plannerbot.planninglabs.nyc/hubot/github-repo-listener"}
+CONFIG = {"url":"http://plannerbot.planninglabs.nyc/hubot/github-repo-listener", "content_type": "json"}
 
-def create_repo_hook(repo):
+def create_hook(repo):
     url = 'https://api.github.com/repos/%s/%s/hooks' % (REPO_OWNER, repo)
     # webhook triggered by all repo events (creation, renamed, etc.)
-    events = ['repository']
+    events = ['repository', 'push']
     params = {"config":CONFIG, "events":events}
     response = requests.post(url, json=params, headers=HEADER)
-
-def create_push_hook(repo):
-    url = 'https://api.github.com/repos/%s/%s/hooks' % (REPO_OWNER, repo)
-    # webhook triggered by push to a repo
-    events = ['push']
-    params = {"config":CONFIG, "events":events}
-    response = requests.post(url, json=params, headers=HEADER) 
-
 
 with open(REPO_LIST) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -35,6 +27,5 @@ with open(REPO_LIST) as csv_file:
             line_count+=1
         else:
             repo_name = row[0]
-            create_repo_hook(repo_name)
-            create_push_hook(repo_name)
+            create_hook(repo_name)
             line_count+=1
